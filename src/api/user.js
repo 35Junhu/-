d94bceef389810be0d093f68b58617fed0b35529
lijunhu035 <3499969408@qq.com>
2022-09-07 17:18:27 +0800

    feat:登录页面完成

---
+++ src/api/user.js
@@ -0,0 +1,19 @@
+import request from '@/utils/request'
+
+// user.js是根据后端给的划分
+export const Login = (mobile, code) => {
+  return request({
+    url: '/v1_0/authorizations',
+    method: 'POST',
+    data: {
+      mobile,
+      code
+    }
+  })
+}
+
+export const sendCodeAPI = (mobile) => {
+  return request({
+    url: `/v1_0/sms/codes/${mobile}`
+  })
+}
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,19 +3,12 @@
     <!-- 头部 -->
     <van-nav-bar title="登录" class="nav-bar" />
     <!-- 表单 -->
-    <van-form @submit="onSubmit" class="form">
+    <van-form @submit="onSubmit" class="form" ref="form">
       <van-field
         v-model="mobile"
         name="mobile"
         placeholder="请输入手机号"
-        :rules="[
-          { required: true, message: '请填写手机号' },
-          {
-            pattern:
-              /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-79])|(?:5[0-35-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[189]))\d{8}$/,
-            message: '手机格式错误'
-          }
-        ]"
+        :rules="mobileRules"
       >
         <template #label>
           <span class="iconfont icon-shouji"></span>
@@ -26,13 +19,30 @@
         type="password"
         name="code"
         placeholder="请输入验证码"
-        :rules="[
-          { required: true, message: '请填写验证码' },
-          { pattern: /^\d{6}$/, message: '手机验证码错误' }
-        ]"
+        :rules="codeRules"
       >
         <template #label>
           <span class="iconfont icon-yanzhengma"></span>
+        </template>
+        <template #button>
+          <van-button
+            type="default"
+            round
+            size="small"
+            block
+            native-type="button"
+            class="btn"
+            v-if="isShowCodeBtn"
+            @click="sendCode"
+            >获取验证码</van-button
+          >
+          <!-- 倒计时 -->
+          <van-count-down
+            :time="3 * 1000"
+            format="ss秒"
+            v-else
+            @finish="isShowCodeBtn = true"
+          />
         </template>
       </van-field>
       <div style="margin: 16px">
@@ -43,16 +53,68 @@
 </template>
 
 <script>
+import { mobileRules, codeRules } from './rule'
+import { Login, sendCodeAPI } from '@/api'
+import { mapMutations } from 'vuex'
 export default {
   data() {
     return {
       mobile: '',
-      code: ''
+      code: '',
+      mobileRules,
+      codeRules,
+      isShowCodeBtn: true
     }
   },
   methods: {
-    onSubmit(values) {
-      console.log('submit', values)
+    ...mapMutations(['SET_TOKEN']),
+    loading() {
+      this.$toast.loading({
+        message: '加载中...',
+        forbidClick: true,
+        duration: 0
+      })
+    },
+    async onSubmit() {
+      this.loading()
+      try {
+        const { data } = await Login(this.mobile, this.code)
+        console.log(data)
+        // 将token存进vuex
+        this.SET_TOKEN(data.data)
+        // 跳转路由
+        this.$router.push('/profile')
+        this.$toast.success('登录成功')
+      } catch (err) {
+        if (err.response && err.response.status === 400) {
+          this.$toast.fail(err.response.data.message)
+        } else {
+          // js导致的错误，抛出错误
+          this.$toast.clear()
+          throw err
+        }
+      }
+    },
+    async sendCode() {
+      await this.$refs.form.validate('mobile')
+      this.loading()
+      // console.log('发送请求')
+      try {
+        await sendCodeAPI(this.mobile)
+        this.isShowCodeBtn = false
+        this.$toast.success('发送验证码成功')
+      } catch (err) {
+        if (
+          (err.response && err.response.status === 429) ||
+          err.response.status === 404
+        ) {
+          // axios错误
+          this.$toast.fail(err.response.data.message)
+        } else {
+          this.$toast.clear()
+          throw err
+        }
+      }
     }
   }
 }
@@ -78,5 +140,11 @@ export default {
   .iconfont {
     font-size: 40px;
   }
+}
+
+.btn {
+  height: 0.64rem;
+  background-color: #eee;
+  color: #a58594;
 }
 </style>
